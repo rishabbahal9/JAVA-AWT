9a07c0756d2814f2d5363f217e3c629aeda4cdb5
Rishab Bahal <rishabbahal@gmail.com>
2018-05-20 22:14:56 +0530

    First Upload

---
+++ MyFrame.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MyFrame
+{
+    public static void main(String[] argmnts) throws IOException
+    {
+        System.out.println("I dont give a fuck.");
+    }
+}
