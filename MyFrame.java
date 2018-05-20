bee289670c95f46e052ec94e83684089115d94c3
rishabbahal <rishabbahal@gmail.com>
2018-05-20 22:36:18 +0530

    Normal Frame is Ready.

--- MyFrame.java
+++ MyFrame.java
@@ -3,10 +3,31 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class MyFrame
+public class MyFrame extends Frame
 {
+    //Constructor
+    MyFrame()
+    {
+        //For closing Window
+        addWindowListener(new WindowAdapter()
+                            {
+                                @Override
+                                public void windowClosing(WindowEvent e) {
+                                    System.exit(0);
+                                }
+
+                            }
+        );
+        //End closing Window
+    }
+    //End Constructor MyFrame()
+
     public static void main(String[] argmnts) throws IOException
     {
         System.out.println("I dont give a fuck to anyone.");
+        MyFrame m=new MyFrame();
+        m.setTitle("Rishab Bahal");
+        m.setSize(300,320);
+        m.setVisible(true);
     }
 }
